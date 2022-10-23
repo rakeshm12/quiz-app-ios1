50aba72cd3867cb133367e01e1d16c0d95bc839e
rakeshm12 <maheshmohan988@gmail.com>
2022-10-23 19:25:24 +0530

    completed module

--- quiz-app-ios/AppDelegate.swift
+++ quiz-app-ios/AppDelegate.swift
@@ -1,13 +1,14 @@
 //
 //  AppDelegate.swift
-//  quiz-app-ios
+//  Quizzler-iOS13
 //
-//  Created by Rakesh Mohan on 23/10/22.
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
