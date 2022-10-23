f7af744dc1067fa72f1f6f820d1c8427c011ce79
rakeshm12 <maheshmohan988@gmail.com>
2022-10-23 19:23:55 +0530

    Initial Commit

---
+++ quiz-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz-app-ios
+//
+//  Created by Rakesh Mohan on 23/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
